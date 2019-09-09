40541d2a0782487baea25ab67dd7301dfd605746
GuoYL <gylgeek@gmail.com>
2019-09-09 14:55:18 +0800

    init code

--- provider/src/main/java/com/huawei/provider/SpringcloudDemoApplication.java
+++ provider/src/main/java/com/huawei/provider/SpringcloudDemoApplication.java
@@ -2,8 +2,12 @@ package com.huawei.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableCircuitBreaker
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringcloudDemoApplication {
 
     public static void main(String[] args) {
